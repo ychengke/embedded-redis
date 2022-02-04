8b92162e032d72fe0d2113ed363b46c73d816b01
ckyang <ckyang@travelsky.com.cn>
2022-02-04 17:32:11 +0800

    将redis.exe动态的存放路径调整为固定路径，以方便设置window防火墙。不然每次启动都需要手动允许一次

--- src/main/java/redis/embedded/util/JarUtil.java
+++ src/main/java/redis/embedded/util/JarUtil.java
@@ -6,11 +6,15 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
 
 public class JarUtil {
 
     public static File extractExecutableFromJar(String executable) throws IOException {
-        File tmpDir = Files.createTempDir();
+        //File tmpDir = Files.createTempDir();
+        //将redis.exe动态的存放路径调整为固定路径，以方便设置window防火墙。不然每次启动都需要手动 “允许” 一次。
+        String tempDirPath = URLDecoder.decode(System.getProperty("user.home"), "UTF-8");
+        File tmpDir = new File(tempDirPath);
         tmpDir.deleteOnExit();
 
         File command = new File(tmpDir, executable);
